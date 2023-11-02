7599f8dcd44c96e27757b790529bfe9c884383fd
jhasanov <jhasanov@gmail.com>
2023-11-02 23:24:20 +0400

    implementing the pixel matching

--- feature_extraction/orb.py
+++ feature_extraction/orb.py
@@ -3,6 +3,7 @@ import imutils
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
+from scipy.spatial.distance import cityblock
 
 fname = sys.argv[1]
 img = cv.imread(fname, cv.IMREAD_GRAYSCALE)
@@ -13,14 +14,33 @@ orb = cv.ORB_create()
 # draw only keypoints location,not size and orientation
 #img2 = cv.drawKeypoints(img, kp, None, color=(0,255,0), flags=0)
 
+matchDict = {}
 
 def drawPoints(img, points):
+	col = (255,0,0)
+	thk = 1
 	img_new = np.stack((img,)*3, axis=-1)
+
 	for coord in points:
-		img_new = cv.circle(img_new,(int(coord[0]),int(coord[1])),5, (255,0,0),2)
+		point = (int(coord[0]),int(coord[1]))
+
+		img_new = cv.circle(img_new,point,5, col,thk)
 	return img_new
 
-# taken from these posts (combination):
+def drawCommonPoints(img, points):
+	img_new = np.stack((img,)*3, axis=-1)
+
+	for coord in points:
+		point = (int(coord[0]),int(coord[1]))
+		col = (255,0,0)
+		thk = 1
+		if point in matchDict:
+			col = (0,255,0)
+			thk = 3
+			img_new = cv.circle(img_new,point,5, col,thk)
+	return img_new
+
+# Credits: taken from these posts (combination):
 # https://stackoverflow.com/questions/30327659/how-can-i-remap-a-point-after-an-image-rotation
 # https://github.com/PyImageSearch/imutils/blob/master/imutils/convenience.py
 def getTransformedPixel(points, angle, scale = 1.0):
@@ -44,6 +64,27 @@ def getTransformedPixel(points, angle, scale = 1.0):
 	# transform points
 	return M.dot(points_ones.T).T
 
+
+# returns the disctionary of nearby points (key) with the match count (value)
+def matchPoints(sorted_list1, sorted_list2, maxDistance = 50):
+	global matchDict
+
+	idx1 = 1
+	idx2 = 1
+
+	while (idx1 < len(sorted_list1)) and (idx2 < len(sorted_list2)):
+		if cityblock(sorted_list1[idx1], sorted_list2[idx2]) < maxDistance:
+			if sorted_list1[idx1] in matchDict:
+				matchDict[sorted_list1[idx1]] += 1
+			else:
+				matchDict[sorted_list1[idx1]] = 1
+
+			idx2 += 1
+		else:
+			idx1 += 1
+
+	return matchDict
+
 img2 = np.copy(img)
 
 rot_p_30 = imutils.rotate_bound(img, angle=30)
@@ -55,14 +96,18 @@ rot_p_45 = imutils.rotate_bound(img, angle=45)
 kp, des = orb.detectAndCompute(img, None)
 
 # Get all the coordinates and convert them to int
-coords = [(int(k.pt[0]),int(k.pt[1])) for k in kp][:20]
+coords = [(int(k.pt[0]),int(k.pt[1])) for k in kp][:100]
+coords_sorted = sorted(coords)
 
 fig, axarr = plt.subplots(2,3)
 
-axarr[0,0].imshow(drawPoints(img2,coords))
-
+kp, des = orb.detectAndCompute(rot_p_30, None)
+coords2 = [(int(k.pt[0]),int(k.pt[1])) for k in kp][:100]
+coords_sorted2 = sorted(coords2)
 trPixels = getTransformedPixel(coords, 30, scale = 1.0)
-axarr[0,1].imshow(drawPoints(rot_p_30, trPixels))
+axarr[0,1].imshow(drawPoints(rot_p_30, coords2))
+md = matchPoints(coords2,trPixels)
+print(md)
 
 trPixels = getTransformedPixel(coords, -30, scale = 1.0)
 axarr[0,2].imshow(drawPoints(rot_n_30, trPixels))
@@ -75,5 +120,7 @@ axarr[1,1].imshow(drawPoints(rot_n_15, trPixels))
 
 trPixels = getTransformedPixel(coords, 45, scale = 1.0)
 axarr[1,2].imshow(drawPoints(rot_p_45, trPixels))
+
+axarr[0,0].imshow(drawCommonPoints(img2,coords))
 
 plt.show()

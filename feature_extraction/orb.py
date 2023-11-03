07685c5722c39113af9206be97a9d449fe38aafa
jhasanov <jhasanov@gmail.com>
2023-11-03 23:03:11 +0400

    added first two screens

--- feature_extraction/orb.py
+++ feature_extraction/orb.py
@@ -19,6 +19,7 @@ matchDict = {}
 def drawPoints(img, points):
 	col = (255,0,0)
 	thk = 1
+
 	img_new = np.stack((img,)*3, axis=-1)
 
 	for coord in points:
@@ -26,6 +27,17 @@ def drawPoints(img, points):
 
 		img_new = cv.circle(img_new,point,5, col,thk)
 	return img_new
+
+def rotateAndDrawPoints(img, angle):
+	col = (255,0,0)
+	thk = 1
+
+	img_new = imutils.rotate_bound(img, angle = angle)
+
+	img_new = np.stack((img_new,)*3, axis=-1)
+	kp, des = orb.detectAndCompute(img_new, None)
+
+	return cv.drawKeypoints(img_new, kp, None, color=(255,0,0), flags=0)
 
 def drawCommonPoints(img, points):
 	img_new = np.stack((img,)*3, axis=-1)
@@ -87,35 +99,22 @@ def matchPoints(sorted_list1, sorted_list2, maxDistance = 50):
 				matchDict[sorted_list1[idx1]] += 1
 			else:
 				matchDict[sorted_list1[idx1]] = 1
-
 			idx2 += 1
 		else:
 			idx1 += 1
-
 	return matchDict
-
-img2 = np.copy(img)
 
 
 kp, des = orb.detectAndCompute(img, None)
 
 # Get all the coordinates and convert them to int
-coords = [(int(k.pt[0]),int(k.pt[1])) for k in kp][:100]
-coords_sorted = sorted(coords)
+coords = [(int(k.pt[0]),int(k.pt[1])) for k in kp]
 
-
-# kp, des = orb.detectAndCompute(rot_p_30, None)
-# coords2 = [(int(k.pt[0]),int(k.pt[1])) for k in kp][:100]
-# coords_sorted2 = sorted(coords2)
-# trPixels = getTransformedPixel(coords, 30, scale = 1.0)
-# axarr[0,1].imshow(drawPoints(rot_p_30, coords2))
-# md = matchPoints(coords2,trPixels)
-# print(md)
 
 # Screen 1: Demonstration of the keypoint transformation
 fig, axarr = plt.subplots(2,3)
 fig.suptitle('1. Demonstration of the keypoint transformation')
-axarr[0,0].imshow(drawPoints(img2,coords))
+axarr[0,0].imshow(drawPoints(img,coords))
 axarr[0,1].imshow(getImageWithTransformedKeypoints(img, 30, coords))
 axarr[0,2].imshow(getImageWithTransformedKeypoints(img, -30, coords))
 axarr[1,0].imshow(getImageWithTransformedKeypoints(img, 15, coords))
@@ -127,10 +126,15 @@ plt.show()
 # Screen 2: Keypoints in various images
 fig, axarr = plt.subplots(2,3)
 fig.suptitle('2. Keypoints in various images')
-axarr[0,0].imshow(drawPoints(img2,coords))
-axarr[0,1].imshow(getImageWithTransformedKeypoints(img, 30, coords))
-axarr[0,2].imshow(getImageWithTransformedKeypoints(img, -30, coords))
-axarr[1,0].imshow(getImageWithTransformedKeypoints(img, 15, coords))
-axarr[1,1].imshow(getImageWithTransformedKeypoints(img, -15, coords))
-axarr[1,2].imshow(getImageWithTransformedKeypoints(img, 45, coords))
+axarr[0,0].imshow(drawPoints(img,coords))
+axarr[0,1].imshow(rotateAndDrawPoints(img, 30))
+axarr[0,2].imshow(rotateAndDrawPoints(img, -30))
+axarr[1,0].imshow(rotateAndDrawPoints(img, 15))
+axarr[1,1].imshow(rotateAndDrawPoints(img, -15))
+axarr[1,2].imshow(rotateAndDrawPoints(img, 45))
 plt.show()
+
+# Screen 3: Showing the 20 common keypoints and the 20x20 regions around them
+coords_sorted = sorted(coords)
+# md = matchPoints(coords2,trPixels)
+# print(md)
